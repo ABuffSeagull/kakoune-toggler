2f967b1573508d9a776eaf3a0364aa8694d45bfc
ABuffSeagull <reecevanatta@gmail.com>
2018-12-07 00:44:30 -0500

    Casing support

--- toggler.cpp
+++ toggler.cpp
@@ -2,12 +2,17 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <locale>
 #include <wordexp.h>
 #include "./cpptoml.h"
 
+bool isTitle = false;
+bool isScream = false;
+std::locale loc("");
+
 void
 find_toggle(std::shared_ptr<cpptoml::table> config, std::string key, std::string word) {
-	std::cerr << "key: " << key << ", word: " << word << std::endl;
+	std::cerr << "key: " << key << ", word: " << word << '.' << std::endl;
 	auto filetype_toggles = config->get_qualified_array_of<cpptoml::array>(key + ".toggles");
 	if (filetype_toggles) {
 		for (const auto &section : *filetype_toggles) {
@@ -20,7 +25,16 @@ find_toggle(std::shared_ptr<cpptoml::table> config, std::string key, std::string
 			if (found == values.end()) {
 				found = values.begin();
 			}
-			std::cout << *found;
+			std::string new_found("");
+			new_found += isTitle ? std::toupper(found->at(0), loc) : found->at(0);
+			if (isScream) {
+				for (const auto &ch : found->substr(1)) {
+					new_found += std::toupper(ch, loc);
+				}
+			} else {
+				new_found += found->substr(1);
+			}
+			std::cout << new_found;
 			exit(0);
 		}
 	}
@@ -35,17 +49,24 @@ main(int argc, char *argv[]) {
 	std::string filetype(argv[1]);
 	std::string word(argv[2]);
 
+	if (std::isupper(word[0], loc)) isTitle = true;
+	if (std::isupper(word[1], loc)) isScream = true;
+	std::string lower_word("");
+	for (auto &ch : word) {
+		lower_word += std::tolower(ch, loc);
+	}
+
 	wordexp_t expanded;
 	wordexp("$HOME/.config/kak/toggles.toml", &expanded, 0);
 	auto config = cpptoml::parse_file(expanded.we_wordv[0]);
 
 	auto extends = config->get_qualified_array_of<std::string>(filetype + ".extends");
 	for (const auto &extend : *extends) {
-		find_toggle(config, extend, word);
+		find_toggle(config, extend, lower_word);
 	}
-	find_toggle(config, filetype, word);
+	find_toggle(config, filetype, lower_word);
 
-	find_toggle(config, "global", word);
+	find_toggle(config, "global", lower_word);
 
 	std::cout << word;
 	std::cerr << "Toggler.kak: Word not found in toggles\n";
--- toggler.kak
+++ toggler.kak
@@ -1,5 +1,5 @@
 define-command toggle %{
-	execute-keys '<a-i>w"ayc'
+	execute-keys '<a-i><a-w>"ayc'
 	execute-keys %sh{
 		$kak_config/bin/toggler $kak_opt_filetype $kak_main_reg_a
 	}<esc>
