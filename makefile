f03d3098bea2455d831338b4c637e3242c9aec90
ABuffSeagull <reecevanatta@gmail.com>
2018-12-07 00:07:22 -0500

    Filetype extending

--- makefile
+++ makefile
@@ -1,2 +1,5 @@
 all:
 	@g++ -Og toggler.cpp -o toggler
+
+debug:
+	@g++ -Wall -O0 toggler.cpp -o toggler
--- toggler.cpp
+++ toggler.cpp
@@ -5,19 +5,10 @@
 #include <wordexp.h>
 #include "./cpptoml.h"
 
-int
-main(int argc, char *argv[]) {
-	if (argc != 3) {
-		std::cerr << "Usage:\n\t$ toggler <filetype> <word>\n";
-		return EXIT_FAILURE;
-	}
-	std::string filetype(argv[1]);
-	std::string word(argv[2]);
-
-	wordexp_t expanded;
-	wordexp("$HOME/.config/kak/toggles.toml", &expanded, 0);
-	auto config = cpptoml::parse_file(expanded.we_wordv[0]);
-	auto filetype_toggles = config->get_array_of<cpptoml::array>(filetype);
+void
+find_toggle(std::shared_ptr<cpptoml::table> config, std::string key, std::string word) {
+	std::cerr << "key: " << key << ", word: " << word << std::endl;
+	auto filetype_toggles = config->get_qualified_array_of<cpptoml::array>(key + ".toggles");
 	if (filetype_toggles) {
 		for (const auto &section : *filetype_toggles) {
 			auto values = *section->get_array_of<std::string>();
@@ -30,26 +21,32 @@ main(int argc, char *argv[]) {
 				found = values.begin();
 			}
 			std::cout << *found;
-			return EXIT_SUCCESS;
+			exit(0);
 		}
 	}
+}
 
-	auto global_toggles = config->get_array_of<cpptoml::array>("global");
-	if (global_toggles) {
-		for (const auto &section : *global_toggles) {
-			auto values = *section->get_array_of<std::string>();
-
-			auto found = std::find(values.begin(), values.end(), word);
-			if (found == values.end()) continue;
-
-			found += 1;
-			if (found == values.end()) {
-				found = values.begin();
-			}
-			std::cout << *found;
-			return EXIT_SUCCESS;
-		}
+int
+main(int argc, char *argv[]) {
+	if (argc != 3) {
+		std::cerr << "Usage:\n\t$ toggler <filetype> <word>\n";
+		return EXIT_FAILURE;
 	}
+	std::string filetype(argv[1]);
+	std::string word(argv[2]);
+
+	wordexp_t expanded;
+	wordexp("$HOME/.config/kak/toggles.toml", &expanded, 0);
+	auto config = cpptoml::parse_file(expanded.we_wordv[0]);
+
+	auto extends = config->get_qualified_array_of<std::string>(filetype + ".extends");
+	for (const auto &extend : *extends) {
+		find_toggle(config, extend, word);
+	}
+	find_toggle(config, filetype, word);
+
+	find_toggle(config, "global", word);
+
 	std::cout << word;
 	std::cerr << "Toggler.kak: Word not found in toggles\n";
 	return EXIT_FAILURE;
