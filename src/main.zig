ac4846a6ad504804dd208703d82f28c9577f87bf
Reece Van Atta <reecevanatta@hey.com>
2020-10-01 19:18:48 -0400

    Tokenize!

--- src/main.zig
+++ src/main.zig
@@ -8,12 +8,17 @@ const Allocator = std.mem.Allocator;
 const HashMap = std.hash_map.StringHashMap;
 
 const LangToggle = struct {
-    extends: ?ArrayList([]u8),
-    toggles: ArrayList(ArrayList([]u8)),
+    extends: ?ArrayList([]const u8),
+    toggles: ArrayList(ArrayList([]const u8)),
 };
 
-pub fn main() anyerror!void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const did_leak = gpa.deinit();
+        std.debug.print("Leaked? {}\n", .{did_leak});
+    }
+    var arena = std.heap.ArenaAllocator.init(&gpa.allocator);
     defer arena.deinit();
     var allocator = &arena.allocator;
 
@@ -34,25 +39,61 @@ pub fn main() anyerror!void {
     // TODO: probably something better than max size
     const contents = try file.inStream().readAllAlloc(allocator, std.math.maxInt(usize));
 
-    const lang_map = try parseLangToggles(allocator, contents);
+    const tokens = try tokenizeTOMLFile(allocator, contents);
+    defer tokens.deinit();
+    for (tokens.items) |token| {
+        switch (token) {
+            .OpenBracket => std.debug.print("Open Bracket\n", .{}),
+            .CloseBracket => std.debug.print("Close Bracket\n", .{}),
+            .EqualSign => std.debug.print("Equal Sign\n", .{}),
+            .Comma => std.debug.print("Comma\n", .{}),
+            .String => |string| std.debug.print("String: {}\n", .{string}),
+            .Identifier => |ident| std.debug.print("Identifier: {}\n", .{ident}),
+        }
+    }
 }
+
+const TOMLToken = union(enum) {
+    OpenBracket,
+    CloseBracket,
+    EqualSign,
+    Comma,
+    String: []const u8,
+    Identifier: []const u8,
+};
 
 const ParseError = error{UnexpectedGlobalKey};
 
-fn parseLangToggles(allocator: *Allocator, file_contents: []const u8) ParseError!HashMap(LangToggle) {
-    const lang_map = HashMap(LangToggle).init(allocator);
-    const first_char_index = eatWhitespace(file_contents, 0);
-    if (file_contents[first_char_index] != '[') return ParseError.UnexpectedGlobalKey;
-    std.debug.warn("first char '{c}' at position {}\n", .{ file_contents[first_char_index], first_char_index });
-    return lang_map;
-}
+fn tokenizeTOMLFile(allocator: *Allocator, file_contents: []const u8) !ArrayList(TOMLToken) {
+    var token_list = ArrayList(TOMLToken).init(allocator);
 
-fn eatWhitespace(buffer: []const u8, position: u64) u64 {
-    var offset: u64 = 0;
-    while (true) {
-        switch (buffer[position + offset]) {
-            ' ', '\t', '\r', '\n' => offset += 1,
-            else => return position + offset,
+    var index: usize = 0;
+    while (index < file_contents.len) : (index += 1) {
+        switch (file_contents[index]) {
+            // ignore whitespace
+            ' ', '\t', '\r', '\n' => {},
+            '[' => try token_list.append(.OpenBracket),
+            ']' => try token_list.append(.CloseBracket),
+            '=' => try token_list.append(.EqualSign),
+            ',' => try token_list.append(.Comma),
+            '"' => {
+                var end_index = index + 1;
+                while (file_contents[end_index] != '"') : (end_index += 1) {}
+                try token_list.append(TOMLToken{ .String = file_contents[index + 1 .. end_index] });
+                index = end_index + 1;
+            },
+            else => |char| {
+                var end_index = index + 1;
+                while (true) : (end_index += 1) {
+                    switch (file_contents[end_index]) {
+                        'a'...'z', 'A'...'Z', '-', '_' => {},
+                        else => break,
+                    }
+                }
+                try token_list.append(TOMLToken{ .Identifier = file_contents[index..end_index] });
+                index = end_index;
+            },
         }
     }
+    return token_list;
 }
